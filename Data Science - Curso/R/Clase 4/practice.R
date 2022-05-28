3b17f40356a4f093723b545e83d54302c4c88daf
Nacho <nachobosch5@gmail.com>
2022-05-28 20:45:56 -0300

    add updates

--- Data Science - Curso/R/Clase 4/practice.R
+++ Data Science - Curso/R/Clase 4/practice.R
@@ -1,6 +1,9 @@
+
+### PRACTICA DATASET TITANIC ###
+
 #Buscamos datasets para practicar --> ?data()
 #Cargamos el dataset
-titanic <- read.csv("./titanic/train.csv")
+titanic <- read.csv("./titanic/train.csv",)
 str(titanic)
 names(titanic)#Nombre de las columnas
 length(titanic)#Cantidad de columnas
@@ -22,6 +25,8 @@ titanic$Sex <- as.factor(titanic$Sex)
 class(titanic$Sex)
 titanic$Embarked <- as.factor(titanic$Embarked)
 class(titanic$Embarked)
+titanic$Pclass <- as.factor(titanic$Pclass)
+class(titanic$Pclass)
 
 #Buscamos na y completamos el dataset por c/columna
 sum(is.na(titanic$Sex))
@@ -37,11 +42,60 @@ titanic$Age[is.na(titanic$Age)] <- age_mean
 sum(is.na(titanic$Age))
 titanic$Age
 
+
 sum(is.na(titanic))# Ahora el dataset está completo
 
 #Hacemos análisis exploratorio mediante gráficas
 library(ggplot2)
+library(patchwork)
 a <- ggplot(titanic)
-a+geom_bar(aes(Sex))
-a+geom_bar(aes(Survived))
+bar_sex<-a+geom_bar(aes(Sex))
+bar_survived <- a+geom_bar(aes(Survived))
+hist_age <- a+geom_histogram(aes(Age))
+bar_sex/bar_survived/hist_age
+a+geom_histogram(aes(titanic$Fare))
+
+a+geom_count(aes(Survived,Age))
+a+geom_boxplot(aes(Sex,Age),fill=Survived)
+a+geom_point(aes(Sex,Age))
+a+geom_bar(aes(Embarked))
+a+geom_bar(aes(Pclass))
+a+geom_point(aes(Age,Pclass))
+a+geom_count(aes(Survived,Sex))
+a+geom_count(aes(Survived,Pclass))
+
+# Hacemos una exploración mediante estadísticos descriptivos
+tabla<-table(titanic$Survived,titanic$Pclass)#construyo table de contigencia
+library(MASS)
+cov(tabla$)
+tabla
+range(titanic$Age)
+min(titanic$Age)
+range(titanic$Fare)
+
+
+#Test de hipotesis
+#Se desea contrastar la hipotesis con un nivel de significancia de 5% que los pasajeros con edad = 30 
+#eran los que No iban a sobrevivir contra los demas si iban a sobrevivir
+#H0: u=30
+#H1: u!=30
+install.packages("BSDA") 
+library(BSDA)
+age <- c(titanic$Age)
+var(age)
+range(age)
+z.test(x=age,y=NULL,alternative="less",mu=30,sigma.x=sd(age),sigma.y=NULL,conf.level=0.95)
+
+#Test de normalidad
+shapiro.test(age)
+
+#Test de hipotesis t-student
+t.test(age,mu=30)
+
+#Test de correlacion entre variables ordinales
+titanic$Survived <- as.numeric(titanic$Survived)
+titanic$Pclass <- as.numeric(titanic$Pclas)
+survivor <- c(titanic$Survived)
+cor.test(titanic$Pclass,survivor,method="spearman") #nos da un p valor menor a 0.05 lo cual rechazamos la H0 de que no existe correlacion
+#existe correlacion negativa entre las clases y los sobrevivientes (esto se puede corroborar graficamente mas arriba)
 
